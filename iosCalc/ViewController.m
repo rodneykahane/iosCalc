8f1b87e5288d30759cfdc14e0b17bac4ae858574
Rodney Kahane <rodney@rodneys-macbook-pro.local>
2017-12-10 14:11:45 -0500

    working code!

--- iosCalc/ViewController.m
+++ iosCalc/ViewController.m
@@ -9,56 +9,43 @@
 #import "ViewController.h"
 #import "Calculator.h"
 
-@interface ViewController ()
-
-@end
-
-@implementation ViewController
-{
+@implementation ViewController {
     char op;
     int currentNumber;
     BOOL firstOperand, isNumerator;
     Calculator *myCalculator;
-    
     NSMutableString *displayString;
-}//end globals
+}
 
+@synthesize display;
 
-
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+-(void) viewDidLoad {
+    //override point for customization after application launch
+    
     firstOperand = YES;
     isNumerator = YES;
     displayString = [NSMutableString stringWithCapacity:40];
     myCalculator = [[Calculator alloc]init];
-}//end viewDidLoad
+}
 
--(void) processDigit: (int) digit {
-
+-(void) processDigit:(int)digit {
     currentNumber = currentNumber * 10 + digit;
     
     [displayString appendString:[NSString stringWithFormat:@"%i",digit]];
-    _display.text=displayString;
-    
-}//end processDigit
+    display.text = displayString;
+}
 
--(IBAction) clickDigit:(UIButton *)sender {
-    
+-(IBAction)clickDigit:(UIButton *) sender {
     int digit = sender.tag;
-    
     [self processDigit:digit];
-    
-}//end clickDigit
+}
 
 -(void) processOp:(char)theOp {
-    
     NSString *opStr;
     
     op = theOp;
     
-    switch(theOp) {
+    switch (theOp) {
         case '+':
             opStr = @" + ";
             break;
@@ -66,113 +53,91 @@
             opStr = @" - ";
             break;
         case '*':
-            opStr = @" * ";
+            opStr = @" x ";
             break;
         case '/':
             opStr = @" / ";
             break;
-    }//end switch
+    }
     
     [self storeFracPart];
     firstOperand = NO;
     isNumerator = YES;
     
-    [displayString appendString: opStr];
-    _display.text = displayString;
-    
-}//end processOp
+    [displayString appendString:opStr];
+    display.text = displayString;
+}
 
 -(void) storeFracPart {
-    
     if (firstOperand) {
-        if(isNumerator) {
-            myCalculator.operand1.numerator = currentNumber;
-            myCalculator.operand1.denominator = 1;  //eg 3* 4/5 =
-        }//end isNumerator
-        else
-            myCalculator.operand1.denominator = currentNumber;
-    }//end if firstOperand
+        if(isNumerator){
+        myCalculator.operand1.numerator = currentNumber;
+        myCalculator.operand1.denominator = 1;
+    }
+    else
+        myCalculator.operand1.denominator = currentNumber;
+    }
     else if (isNumerator) {
         myCalculator.operand2.numerator = currentNumber;
-        myCalculator.operand2.denominator = 1; // eg 3/2 * 4 =
-    }//end else if
+        myCalculator.operand2.denominator = 1;
+    }
     else {
         myCalculator.operand2.denominator = currentNumber;
         firstOperand = YES;
-    }//end else
+    }
     
     currentNumber = 0;
-    
-}//end storeFracPart
+}
 
 -(IBAction)clickOver {
     [self storeFracPart];
     isNumerator = NO;
-    [displayString appendString: @"/"];
-    _display.text = displayString;
-}//end clickOver
+    [displayString appendString:@"/"];
+    display.text=displayString;
+}
 
 -(IBAction)clickPlus {
-    
-    [self processOp: '+'];
-    
-}//end clickPlus
+    [self processOp:'+'];
+}
 
 -(IBAction)clickMinus {
-    
     [self processOp:'-'];
-    
-}//end clickMinus
+}
 
 -(IBAction)clickMultiply {
-    
     [self processOp:'*'];
-    
-}//end clickMultiply
+}
 
 -(IBAction)clickDivide {
-    
     [self processOp:'/'];
-    
-}//end clickDivide
-
-//Misc. keys
+}
 
 -(IBAction)clickEquals {
-    
     if (firstOperand == NO) {
         [self storeFracPart];
         [myCalculator performOperation:op];
         
-        [displayString appendString: @" = "];
+        [displayString appendString:@" = "];
         [displayString appendString:[myCalculator.accumulator convertToString]];
-        _display.text = displayString;
+        display.text = displayString;
         
         currentNumber = 0;
         isNumerator = YES;
         firstOperand = YES;
+        
         [displayString setString:@""];
     }
-    
-}//end clickEquals
+}
 
 -(IBAction)clickClear {
+    isNumerator = YES;
     firstOperand = YES;
     currentNumber = 0;
     [myCalculator clear];
     
     [displayString setString:@""];
-    _display.text = displayString;
-    
-    isNumerator = YES;
-    
-    
-}//end clickClear
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}//end didReceiveMemoryWarning
-
+    display.text = displayString;
+}
 
 @end
+
